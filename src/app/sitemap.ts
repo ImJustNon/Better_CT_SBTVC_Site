2fde674efe992afe094e4b289739278f7a4fd0ca
ImJustNon <Im_Just_Non@hotmail.com>
2025-03-14 19:48:52 +0700

    Upadate Robots and Sitemap

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,14 +1,18 @@
-import { MetadataRoute } from "next";
-
+import type { MetadataRoute } from 'next'
+ 
 export default function sitemap(): MetadataRoute.Sitemap {
   return [
     {
-      url: "https://ct-sbtvc.vercel.app/",
-      lastModified: new Date().toISOString(),
+      url: 'https://ct-sbtvc.vercel.app',
+      lastModified: new Date(),
+      changeFrequency: 'monthly',
+      priority: 1,
     },
     {
-      url: "https://ct-sbtvc.vercel.app/about",
-      lastModified: new Date().toISOString(),
+      url: 'https://ct-sbtvc.vercel.app/about',
+      lastModified: new Date(),
+      changeFrequency: 'yearly',
+      priority: 0.8,
     },
-  ];
+  ]
 }
