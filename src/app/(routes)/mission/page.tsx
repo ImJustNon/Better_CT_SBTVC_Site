b0a7930b0ec98adddf91f790687069a097422adb
ImJustNon <Im_Just_Non@hotmail.com>
2025-06-15 20:36:59 +0700

    Update Use Client Why????

--- src/app/(routes)/mission/page.tsx
+++ src/app/(routes)/mission/page.tsx
@@ -1,4 +1,4 @@
-"use-client"
+"use client"
 
 import NavigationbarComponent from "@/components/Navigationbar.component";
 import VideoBackgroundComponent from "@/components/VideoBackground.component";
